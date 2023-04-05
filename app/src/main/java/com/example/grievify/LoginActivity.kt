d21daf474738a58a64ad663ec45a5e373ae64b62
Navneet Raj Karn <97835851+navneet098@users.noreply.github.com>
2023-04-05 15:10:53 +0530

    Add: LogIn Activity

--- app/src/main/java/com/example/grievify/LoginActivity.kt
+++ app/src/main/java/com/example/grievify/LoginActivity.kt
@@ -3,12 +3,15 @@ package com.example.grievify
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.grievify.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
 
     lateinit var loginBinding:ActivityLoginBinding
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loginBinding= ActivityLoginBinding.inflate(layoutInflater)
@@ -19,5 +22,35 @@ class LoginActivity : AppCompatActivity() {
             val intent= Intent(this@LoginActivity,SignUpActivity::class.java)
             startActivity(intent)
         }
+
+        loginBinding.loginButton.setOnClickListener {
+            val userEmail=loginBinding.email.text.toString()
+            val userPassword=loginBinding.password.text.toString()
+            logIn(userEmail, userPassword )
+        }
+    }
+
+    fun logIn(userEmail:String,userPassword:String){
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(
+                        applicationContext,
+                        "Login is successful",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    val intent=Intent(this@LoginActivity,MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        applicationContext,
+                        task.exception?.toString(),
+                        Toast.LENGTH_LONG)
+
+                }
+            }
     }
 }
