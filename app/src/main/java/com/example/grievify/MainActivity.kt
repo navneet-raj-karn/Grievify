c3e4c4704de8d9d04f84a0c2f249785a832cb216
AS-Saikia <arpitsaikia03@gmail.com>
2023-04-06 01:43:10 +0530

    ChatBot implemented

--- app/src/main/java/com/example/grievify/MainActivity.kt
+++ app/src/main/java/com/example/grievify/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.grievify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val chatBotActivate: ExtendedFloatingActionButton = findViewById(R.id.sell_button)
+        chatBotActivate.setOnClickListener {
+            startActivity(Intent(this, ChatBot::class.java))
+        }
     }
+
 }
