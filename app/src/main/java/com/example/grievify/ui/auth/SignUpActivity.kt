21fa3123d9e2bae493c72a009f628338e34be749
Navneet Raj Karn <97835851+navneet098@users.noreply.github.com>
2023-04-07 01:57:47 +0530

    Add: User Data

--- app/src/main/java/com/example/grievify/ui/auth/SignUpActivity.kt
+++ app/src/main/java/com/example/grievify/ui/auth/SignUpActivity.kt
@@ -1,14 +1,22 @@
 package com.example.grievify.ui.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.grievify.MainActivity
 import com.example.grievify.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
     lateinit var signUpBinding: ActivitySignUpBinding
     val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    val database:FirebaseDatabase=FirebaseDatabase.getInstance()
+    val reference:DatabaseReference=database.reference.child("Users")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         signUpBinding = ActivitySignUpBinding.inflate(layoutInflater)
@@ -30,6 +38,20 @@ class SignUpActivity : AppCompatActivity() {
                     "Your account has been created",
                     Toast.LENGTH_SHORT
                 ).show()
+                val user= Firebase.auth.currentUser?.uid.toString()
+                var name=signUpBinding.userName.text.toString()
+                var scholarId=signUpBinding.scholarId.text.toString()
+                val userEmail = signUpBinding.email.text.toString()
+                reference.child(user).child("name").setValue(name)
+                reference.child(user).child("scholarId").setValue(scholarId)
+                reference.child(user).child("email").setValue(userEmail)
+                val intent= Intent(this@SignUpActivity, LoginActivity::class.java)
+                startActivity(intent)
+
+
+
+
+
             } else {
                 Toast.makeText(
                     applicationContext,
