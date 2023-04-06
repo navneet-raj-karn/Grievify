0d0909208c75ed628c13ee2ad06dc2eb4d7ebede
Navneet Raj Karn <97835851+navneet098@users.noreply.github.com>
2023-04-07 03:22:53 +0530

    Add: Delete Button

--- app/src/main/java/com/example/grievify/adapters/ComplaintAdapter.kt
+++ app/src/main/java/com/example/grievify/adapters/ComplaintAdapter.kt
@@ -4,9 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.grievify.data.TicketData
 import com.example.grievify.databinding.ComplaintBoxBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
 
 class ComplaintAdapter(
     private val context: Context?,
@@ -31,6 +36,39 @@ class ComplaintAdapter(
         holder.adapterBinding.Subject.text=itemList[position].title
         holder.adapterBinding.status.text=itemList[position].status
         holder.adapterBinding.ticket.text=itemList[position].ticketID
+        holder.adapterBinding.deleteButton.setOnClickListener {
+            val builder = AlertDialog.Builder(context!!)
+            builder.setTitle("Are you sure?")
+            builder.setMessage("Your item will be deleted permanently from the database")
+            builder.setPositiveButton("Yes"){_,_->
+                deleteModel(itemList[position].ticketID.toString())
+
+            }
+        }
+
+    }
+
+    private fun deleteModel(model: String) {
+        val databaseProd =
+            FirebaseDatabase.getInstance().getReference("tickets")
+        databaseProd.child(model).removeValue().addOnSuccessListener{
+            val ref = FirebaseDatabase.getInstance().getReference("tickets")
+            val query: Query = ref.orderByValue().equalTo(model)
+            query.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    for (snapshot in dataSnapshot.children) {
+                        snapshot.ref.removeValue()
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                }
+            })
+            Toast.makeText(context, "Item deleted from database", Toast.LENGTH_LONG).show()
+
+        }.addOnFailureListener {
+            Toast.makeText(context, "Error", Toast.LENGTH_LONG).show()
+        }
 
     }
 
