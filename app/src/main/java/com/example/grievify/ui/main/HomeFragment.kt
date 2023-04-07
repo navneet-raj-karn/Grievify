1471f84ce9e2d27ca6826f1329096c1ec6dfad96
Deep Saikia <96719584+saikiaDeep@users.noreply.github.com>
2023-04-07 08:50:57 +0530

    Add:Home Fragment

---
+++ app/src/main/java/com/example/grievify/ui/main/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.grievify.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.grievify.R
+import com.example.grievify.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+
+    }
+
+
+}
