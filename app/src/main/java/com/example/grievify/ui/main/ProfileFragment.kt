cb966ac096ce61d3618b9515802c443dbd586ab1
Deep Saikia <96719584+saikiaDeep@users.noreply.github.com>
2023-04-07 09:41:35 +0530

    Profile Fragment UI updated

--- app/src/main/java/com/example/grievify/ui/main/ProfileFragment.kt
+++ app/src/main/java/com/example/grievify/ui/main/ProfileFragment.kt
@@ -1,14 +1,19 @@
 package com.example.grievify.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.grievify.CreateComplaint
+import com.example.grievify.MainActivity
 import com.example.grievify.R
 import com.example.grievify.data.UserData
 import com.example.grievify.databinding.FragmentComplainListBinding
 import com.example.grievify.databinding.FragmentProfileBinding
+import com.example.grievify.ui.auth.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
@@ -28,6 +33,12 @@ class ProfileFragment : Fragment() {
     ): View? {
         viewBinding = FragmentProfileBinding.inflate(inflater, container, false)
         retriveDataFromDatabase()
+        binding.buttonLogOut.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
         return binding.root
     }
 
@@ -38,10 +49,10 @@ class ProfileFragment : Fragment() {
                 val user = snapshot.child(Firebase.auth.currentUser?.uid.toString()).getValue(
                     UserData::class.java)
                 if (user != null){
-                    binding.email.text=user.email
+                    binding.EMAIL.text=user.email
                     println("Hello ${user.name}")
-                    binding.name.text=user.name
-                    binding.scholarId.text=user.scholarId
+                    binding.USERNAME.text=user.name
+                    binding.dept.text=user.scholarId
                 }
 
 
