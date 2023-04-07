d01775683cdd396508be04f9aa52dd75f3dd050e
Deep Saikia <96719584+saikiaDeep@users.noreply.github.com>
2023-04-07 08:01:40 +0530

    Main Activity button add

--- app/src/main/java/com/example/grievify/MainActivity.kt
+++ app/src/main/java/com/example/grievify/MainActivity.kt
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.example.grievify.databinding.ActivityMainBinding
 import com.example.grievify.ui.auth.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.register.setOnClickListener {
+            Toast.makeText(applicationContext,"clicked",Toast.LENGTH_SHORT).show()
+        }
+        binding.list.setOnClickListener {
+            Toast.makeText(applicationContext,"clicked",Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,6 +33,11 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId==R.id.signOut){
             FirebaseAuth.getInstance().signOut()
+            val intent= Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        if(item.itemId==R.id.proflie){
             val intent= Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
